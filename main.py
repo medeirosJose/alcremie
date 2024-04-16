dfd8696dd3e25e1ad82260d1eaa01ce79687032c
medeirosJose <joseeduardomj@gmail.com>
2024-04-16 01:38:11 -0300

    adding icons to buttons in sidebar + icons folder

--- main.py
+++ main.py
@@ -3,83 +3,113 @@ from PIL import Image, ImageTk
 from views.order_view import OrderView
 from views.products_view import ProductsView
 from views.customer_view import CustomersView
-from controllers.app_controller import (
-    AppController,
-)
+from controllers.app_controller import AppController
 
 
 class Main(tk.Tk):
     def __init__(self, app_controller):
         super().__init__()
         self.title("Confeitaria Alcremie")
-        self.state("zoomed")  # garante que a janela inicie maximizada
+        self.state("zoomed")
 
-        # cria a barra lateral
-        self.sidebar = tk.Frame(self, width=400, bg="gray")
-        self.sidebar.pack(fill="y", side="left")
+        self.sidebar = tk.Frame(self, width=100, bg="gray")
+        self.sidebar.pack(
+            fill="y",
+            side="left",
+        )
 
         self.load_and_display_logo()
+        self.load_icons()
 
-        # cria o container para as views
         self.container = tk.Frame(self)
         self.container.pack(fill="both", expand=True)
 
-        # cria a imagem de logo
-        image = Image.open("logo.png")
-        photo = ImageTk.PhotoImage(image)
-        label = tk.Label(self.container, image=photo)
-        label.image = photo
-        # centraliza a imagem no centro da view do container
-        label.place(relx=0.5, rely=0.5, anchor="center")
-
-        # cria o controller principal (singleton)
         self.app_controller = app_controller
-        self.user_controller = self.app_controller.get_user_controller()
 
         #! Aqui tem q adicionar as demais views e seus controllers atraves do self.app_controller
         self.views = {
-            "Pedido": (OrderView, self.app_controller.get_order_controller()),
-            "Produtos": (ProductsView, self.app_controller.get_products_controller()),
-            "Clientes": (CustomersView, self.app_controller.get_customer_controller()),
+            "Pedido": (
+                OrderView,
+                self.app_controller.get_order_controller(),
+                self.icons["order"],
+            ),
+            "Produtos": (
+                ProductsView,
+                self.app_controller.get_products_controller(),
+                self.icons["products"],
+            ),
+            "Clientes": (
+                CustomersView,
+                self.app_controller.get_customer_controller(),
+                self.icons["customers"],
+            ),
             # placeholder
-            "Relatórios": (OrderView, self.app_controller.get_order_controller()),
-            "Configurações": (OrderView, self.app_controller.get_order_controller()),
-            "Sair": (OrderView, self.app_controller.get_order_controller()),
+            "Relatórios": (
+                OrderView,
+                self.app_controller.get_order_controller(),
+                self.icons["reports"],
+            ),
+            "Pagamentos": (
+                OrderView,
+                self.app_controller.get_order_controller(),
+                self.icons["settings"],
+            ),
+            "Sair": (
+                OrderView,
+                self.app_controller.get_order_controller(),
+                self.icons["order"],
+            ),
         }
 
         self.current_view = None
         self.buttons = {}
-        self.active_button = None
         self.create_sidebar_buttons()
 
+    # TODO Trocar o diretorio das funcoes abaixo para o diretorio correto
+    def load_icons(self):
+        self.icons = {
+            "order": ImageTk.PhotoImage(
+                Image.open("icons/order2.png").resize((24, 24))
+            ),
+            "products": ImageTk.PhotoImage(
+                Image.open("icons/product2.png").resize((24, 24))
+            ),
+            "customers": ImageTk.PhotoImage(
+                Image.open("icons/customer2.png").resize((24, 24))
+            ),
+            "reports": ImageTk.PhotoImage(
+                Image.open("icons/reports2.png").resize((24, 24))
+            ),
+            "settings": ImageTk.PhotoImage(
+                Image.open("icons/payment.png").resize((24, 24))
+            ),
+        }
+
+    # serve para carregar a logo da aplicacao acima dos botoes da sidebar
     def load_and_display_logo(self):
-        logo_image = Image.open("logo.png")
-        logo_image = logo_image.resize((150, 150), Image.LANCZOS)
-
-        logo_photo = ImageTk.PhotoImage(logo_image)
-        logo_label = tk.Label(self.sidebar, image=logo_photo, bg="gray")
-        logo_label.image = logo_photo
-        logo_label.pack(pady=20)
-
-        # divisória
-        divider = tk.Frame(
-            self.sidebar,
-            height=1,
-        )
-        divider.pack(fill="x", padx=50, pady=10)
+        logo_image = ImageTk.PhotoImage(Image.open("icons/logo.png").resize((150, 150)))
+        logo_label = tk.Label(self.sidebar, image=logo_image, bg="gray")
+        logo_label.image = logo_image
+        logo_label.pack(pady=10)
+        tk.Frame(self.sidebar, height=1, bg="WHITE").pack(fill="x", padx=10, pady=10)
 
     # funcao que cria os botoes da barra lateral e associa a visualizacao correspondente
     def create_sidebar_buttons(self):
-        for view_name in self.views:
+        for view_name, (view_class, controller, icon) in self.views.items():
             button = tk.Button(
                 self.sidebar,
+                image=icon,
                 text=view_name,
+                font=("Roboto", 10),
+                compound="left",
                 command=lambda name=view_name: self.show_view(name),
-                relief="ridge",
-                bg="white",
-                width=20,
+                relief="flat",
+                anchor="w",
+                width=150,
+                height=30,
+                padx=20,
             )
-            button.pack(padx=10, pady=10, fill="x")
+            button.pack(pady=10, padx=10, fill="x")
             self.buttons[view_name] = button
 
     # funcao que exibe a visualizacao clicada
@@ -88,22 +118,19 @@ class Main(tk.Tk):
             # se houver, destroi a visualização atual
             self.current_view[0].destroy()
 
-        if self.active_button:
-            self.active_button.config(bg="white")
-
         # recupera a classe e o controlador da visualização selecionada
-        view_class, controller = self.views[view_name]
+        view_class, controller, _ = self.views[view_name]
 
         # instancia a view selecionada, passando o container e o controlador
         view = view_class(self.container, controller)
 
         view.pack(fill="both", expand=True)
-
         self.current_view = (view, controller)
 
-        # atualiza o botão ativo
-        self.active_button = self.buttons[view_name]
-        self.active_button.config(bg="#faebd8")
+        # muda a cor do botão clicado
+        for btn in self.buttons.values():
+            btn.config(bg="white")
+        self.buttons[view_name].config(bg="#faebd8")
 
 
 if __name__ == "__main__":
